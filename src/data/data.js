23479c8e0e784bd943a064c5c4de2cfd5eeed43f
yigitden <60812730+yigitden@users.noreply.github.com>
2022-01-10 11:40:53 +0300

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,4 @@
-const data =  {
-  
+export data {
   name: "Yigit",
   job: "Front-End Developer",
   text_main:"It's my portfolio site and I created this site with React",
@@ -88,5 +87,3 @@ const data =  {
   ]
 }
 
-
-export default data;
